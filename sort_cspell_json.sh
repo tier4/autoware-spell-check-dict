f65c753e99d25014ca5136332a2fd70b2cf26864
Kenji Miyake <31987104+kenji-miyake@users.noreply.github.com>
2021-05-18 12:41:07 +0900

    Add a script and GitHub action to sort the cspell.json (#2)
    * Remove comment
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Add sort script
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Add github action for sorting
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Add ref
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Update .github/workflows/sort.yml
    
    Co-authored-by: Kazuki Miyahara <kmiya@outlook.com>
    
    * Update sort.sh
    
    Co-authored-by: Kazuki Miyahara <kmiya@outlook.com>
    
    * Rename sort.sh to sort_cspell_json.sh
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    Co-authored-by: Kazuki Miyahara <kmiya@outlook.com>

---
+++ sort_cspell_json.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(readlink -e "$(dirname "${0}")")
+
+# Install jq
+if ! (command -v jq > /dev/null 2>&1); then
+    sudo apt-get install -y jq
+fi
+
+# Set target file
+cspell_json="$SCRIPT_DIR/cspell/.cspell.json"
+
+# Create new json
+sorted_json=$(jq --indent 4 \
+    "{
+        version: .version,
+        language: .language,
+        allowCompoundWords: .allowCompoundWords,
+        languageSettings: .languageSettings,
+        ignorePaths: .ignorePaths | sort_by(.),
+        words: .words | unique | sort_by(. | ascii_downcase),
+    }" < "$cspell_json"
+)
+
+# If jq succeed, replace file
+if [ "${PIPESTATUS[0]}" = "0" ]; then
+    echo "$sorted_json" > "$cspell_json"
+else
+    echo "Failed to parse json."
+fi
