fb652dd8c6a614cb72e1c91e768627be6693bd79
Keisuke Shima <19993104+KeisukeShima@users.noreply.github.com>
2022-03-25 10:40:51 +0900

    remove NamedTemporaryFile
    Signed-off-by: Keisuke Shima <19993104+KeisukeShima@users.noreply.github.com>

--- tests/test_sort_cspell_json.py
+++ tests/test_sort_cspell_json.py
@@ -1,16 +1,15 @@
 from pathlib import Path
 import shutil
-from tempfile import NamedTemporaryFile
 
 from pre_commit_hooks import sort_cspell_json
 
 
-def test(datadir: Path):
+def test(tmp_path: Path, datadir: Path):
     input_file = datadir / ".cspell.input.json"
     answer_file = datadir / ".cspell.answer.json"
 
     # Create a temporary file to prevent overwriting original files
-    tmp_file = Path(NamedTemporaryFile(mode="w", suffix=".json").name)
+    tmp_file = tmp_path / ".cspell.input.json"
     shutil.copy(input_file, tmp_file)
 
     # Format
