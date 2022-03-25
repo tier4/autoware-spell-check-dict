d05fd584b95dfc7602e08fb248790cee89c76575
Keisuke Shima <19993104+KeisukeShima@users.noreply.github.com>
2022-03-25 11:23:00 +0900

    change to use input_file directory
    Signed-off-by: Keisuke Shima <19993104+KeisukeShima@users.noreply.github.com>

--- tests/test_format_cspell_json.py
+++ tests/test_format_cspell_json.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-import shutil
 
 from pre_commit_hooks import format_cspell_json
 
@@ -8,16 +7,12 @@ def test(datadir: Path):
     input_file = datadir / ".cspell.input.json"
     answer_file = datadir / ".cspell.answer.json"
 
-    # Create a temporary file to prevent overwriting original files
-    target_file = input_file.with_suffix(".target")
-    shutil.copy(input_file, target_file)
-
     # Format
-    return_code = format_cspell_json.main([str(target_file)])
+    return_code = format_cspell_json.main([str(input_file)])
     assert return_code == 1
-    assert target_file.read_text() == answer_file.read_text()
+    assert input_file.read_text() == answer_file.read_text()
 
     # Re-format
-    return_code = format_cspell_json.main([str(target_file)])
+    return_code = format_cspell_json.main([str(input_file)])
     assert return_code == 0
-    assert target_file.read_text() == answer_file.read_text()
+    assert input_file.read_text() == answer_file.read_text()
