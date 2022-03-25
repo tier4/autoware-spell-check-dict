a391926cf70fd622aaa53027ac109d49c9d5c464
Keisuke Shima <19993104+KeisukeShima@users.noreply.github.com>
2022-03-25 11:19:09 +0900

    rename to format_cspell_json
    Signed-off-by: Keisuke Shima <19993104+KeisukeShima@users.noreply.github.com>

--- tests/test_format_cspell_json.py
+++ tests/test_format_cspell_json.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import shutil
 
-from pre_commit_hooks import sort_cspell_json
+from pre_commit_hooks import format_cspell_json
 
 
 def test(datadir: Path):
@@ -13,11 +13,11 @@ def test(datadir: Path):
     shutil.copy(input_file, target_file)
 
     # Format
-    return_code = sort_cspell_json.main([str(target_file)])
+    return_code = format_cspell_json.main([str(target_file)])
     assert return_code == 1
     assert target_file.read_text() == answer_file.read_text()
 
     # Re-format
-    return_code = sort_cspell_json.main([str(target_file)])
+    return_code = format_cspell_json.main([str(target_file)])
     assert return_code == 0
     assert target_file.read_text() == answer_file.read_text()
