57d89543311c215c4f0ee515b61798b13e170be7
Kenji Miyake <kenji.miyake@tier4.jp>
2022-04-16 23:03:39 +0900

    ci: refactor format_cspell_json.py
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

--- format_cspell_json/format_cspell_json.py
+++ format_cspell_json/format_cspell_json.py
@@ -17,6 +17,7 @@ import copy
 import json
 from pathlib import Path
 from typing import Dict
+from typing import List
 from typing import Optional
 from typing import Sequence
 
@@ -33,30 +34,72 @@ def format_json_recursive(j: Dict) -> Dict:
     return j
 
 
+def sort_dict_keys(d: Dict, new_keys: List[str]):
+    reorganized = {}
+    for key in new_keys:
+        if d.get(key) is not None:
+            reorganized[key] = d[key]
+    return reorganized
+
+
 def format_cspell_json(cspell_json: Dict) -> Dict:
     # Sort inside each key
     formatted = format_json_recursive(cspell_json)
 
     # Sort top-level keys
-    new_keys = [
-        "version",
-        "language",
-        "allowCompoundWords",
-        "ignorePaths",
-        "ignoreRegExpList",
-        "overrides",
-        "flagWords",
-        "words",
-    ]
-    reorganized = {}
-    for key in new_keys:
-        if formatted.get(key) is not None:
-            reorganized[key] = formatted[key]
+    formatted = sort_dict_keys(
+        formatted,
+        [
+            "version",
+            "language",
+            "allowCompoundWords",
+            "ignorePaths",
+            "ignoreRegExpList",
+            "languageSettings",
+            "overrides",
+            "flagWords",
+            "words",
+        ],
+    )
+
+    # Sort inside "languageSettings"
+    if formatted.get("languageSettings") is not None:
+        # Sort items
+        formatted["languageSettings"] = sorted(
+            formatted["languageSettings"], key=lambda x: x["languageId"]
+        )
+
+        # Sort keys
+        formatted["languageSettings"] = [
+            sort_dict_keys(
+                d,
+                [
+                    "languageId",
+                    "dictionaries",
+                    "ignoreRegExpList",
+                ],
+            )
+            for d in formatted["languageSettings"]
+        ]
 
     # Sort inside "overrides"
-    reorganized["overrides"] = sorted(reorganized["overrides"], key=lambda x: x["filename"])
+    if formatted.get("overrides") is not None:
+        # Sort items
+        formatted["overrides"] = sorted(formatted["overrides"], key=lambda x: x["filename"])
 
-    return reorganized
+        # Sort keys
+        formatted["overrides"] = [
+            sort_dict_keys(
+                d,
+                [
+                    "filename",
+                    "ignoreRegExpList",
+                ],
+            )
+            for d in formatted["overrides"]
+        ]
+
+    return formatted
 
 
 def main(argv: Optional[Sequence[str]] = None) -> int:
@@ -66,14 +109,15 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
 
     return_code = 0
     for filepath in args.filenames:
-        original_json = json.loads(filepath.read_text(encoding="utf-8"))
-        sorted_json = format_cspell_json(copy.deepcopy(original_json))
+        original_json_str = filepath.read_text(encoding="utf-8")
+        original_json = json.loads(original_json_str)
 
-        if json.dumps(sorted_json) != json.dumps(original_json):
+        sorted_json = format_cspell_json(copy.deepcopy(original_json))
+        sorted_json_str = json.dumps(sorted_json, indent=2, ensure_ascii=False) + "\n"
+
+        if sorted_json_str != original_json_str:
             print(f"Fixing {filepath}")
-            filepath.write_text(
-                json.dumps(sorted_json, indent=2, ensure_ascii=False) + "\n", encoding="utf-8"
-            )
+            filepath.write_text(sorted_json_str, encoding="utf-8")
             return_code = 1
 
     return return_code
